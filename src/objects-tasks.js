938cd29bd17d2e78962216a400ec13cbf6fe1c43
Sergey <sk85post@gmail.com>
2024-03-03 16:16:31 +0300

    feat: complete makeWord

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -135,8 +135,16 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const arr = [];
+  const entries = Object.entries(lettersObject);
+  entries.forEach((item) => {
+    const [key, values] = item;
+    values.forEach((val) => {
+      arr[val] = key;
+    });
+  });
+  return arr.join('');
 }
 
 /**
