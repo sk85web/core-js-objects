67a4432ff167f8fb701d70235616cf5cb4565a42
Sergey <sk85post@gmail.com>
2024-03-03 16:33:00 +0300

    feat: complete sellTickets

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -161,8 +161,12 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  if (queue.length === 0) return true;
+  const end = queue[queue.length - 1];
+  const changeArr = queue.slice(0, queue.length - 1);
+  const sum = changeArr.reduce((acc, cur) => acc + cur, 0);
+  return sum >= end;
 }
 
 /**
